5d27b875e18a084b059b014747fc455043a99e47
shdbcks <rednyc@snu.ac.kr>
2024-06-07 17:28:19 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -7,12 +7,20 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
+	if(cnt == M){
+		for(int i = 0; i < M; i++){
+			cout << vec[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+	
+	int point = (vec.empty() ? 1: vec.back() + 1);
+	for(int i = point; i <=N; i++){
+		vec.push_back(i);
+		func(cnt + 1);
+		vec.pop_back();
+	}
 
 }
 
